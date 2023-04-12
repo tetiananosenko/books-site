454d55442c3b8f62d885472b4b47ef3a1b1a1942
Tetiana Nosenko <tanya2016english@gmail.com>
2023-04-12 18:25:07 +0200

    change function

--- src/js/script.js
+++ src/js/script.js
@@ -24,40 +24,39 @@ class BooksList {
     }
   }
   initActions() {
-    const bookImgs = document.querySelectorAll('.book__image');
-    for (let element of bookImgs) {
-      console.log(bookImgs);
-      element.addEventListener('dblclick', function (event) {
-        event.preventDefault();
-        if (event.target.offsetParent.classList.contains('book__image')) {
-          element.classList.toggle('favorite');
-        }
-      });
-    }
+    const booksList = document.querySelector('.books-list');
+    booksList.addEventListener('dblclick', function (event) {
+      event.preventDefault();
+      if (event.target.offsetParent.classList.contains('book__image')) {
+        event.target.offsetParent.classList.toggle('favorite');
+      }
+    });
     const filtersForm = document.querySelector('.filters');
     let filters = [];
-    const fn = function (event) {
+    const fn = (event) => {
       if (event.target.value === 'adults' && event.target.checked) {
         filters.push(event.target.value);
       } else if (event.target.value === 'nonFiction' && event.target.checked) {
         filters.push(event.target.value);
-      } else {
+      } else if (event.target.name === 'filter') {
         let filterIndex = filters.indexOf(event.target.value);
+        console.log(event.target.value);
         filters.splice(filterIndex, 1);
       }
       this.filterBooks(dataSource.books, filters);
     }
-    filtersForm.addEventListener('click', fn.bind(this));
+    filtersForm.addEventListener('click', fn);
   }
   filterBooks(books, filters) {
     for (let book of books) {
-      let shouldBeHidden = false;
-      for (let filter of filters) {
-        if (book.details[filter] === true) {
-          shouldBeHidden = true;
-          break;
-        }
-      }
+      // let shouldBeHidden = false;
+      const shouldBeHidden = filters.some(filter => !book.details[filter]);
+      // for (let filter of filters) {
+      //   if (book.details[filter] === true) {
+      //     shouldBeHidden = true;
+      //     break;
+      //   }
+      // }
       const bookID = document.querySelector(`.book__image[data-id='${book.id}']`);
       if (shouldBeHidden) {
         bookID.classList.add('hidden');
@@ -69,15 +68,15 @@ class BooksList {
   determinRatingBgc(booksRating) {
     switch (booksRating) {
       case booksRating < 6:
-        return 'linear-gradient(to bottom, #fefcea 0 %, #f1da36 100 %)';
+        return 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%)';
       case booksRating > 6 && booksRating <= 8:
-        return 'linear-gradient(to bottom, #fefcea 0 %, #f1da36 100 %)';
+        return 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%)';
       case booksRating > 8 && booksRating <= 9:
         return 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
       case booksRating > 9:
         return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
       default:
-        return 'linear-gradient(to bottom, #fefcea 0 %, #f1da36 100 %)';
+        return 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%)';
     }
   }
 }
